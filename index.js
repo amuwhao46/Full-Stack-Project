863abce7eaab456a317a6e2ca31d7ab58ee1362c
Oke Amuwha <amuwhao46@gmail.com>
2022-09-29 15:27:39 -0400

    Added automatic loader for data wrapper

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 const API = {
     async getRandomDog() {
         fetch("https://random.dog/woof.json").then(response => response.json()).then(function (data) {
-            const dog = data.url
+            const dog = data.url //Handles dog pic or video
+            const isVideo = dog.split('.').pop() == "mp4" //splits last element in array
+
+            document.querySelector("#dog").setAttribute("src", dog)
         })
     },
+}
+
+window.onload = function() {
+    API.getRandomDog()
 }
