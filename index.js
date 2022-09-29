d62569bc4c3581e7439db472fa6cf78426658919
Oke Amuwha <amuwhao46@gmail.com>
2022-09-29 15:00:30 -0400

    Finished data wrapper and contextualized main html page

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // API Wrapper
 const API = {
-    getRandomDog() {
-
+    async getRandomDog() {
+        try {
+            const response = await fetch("https://random.dog/woof.json");
+            const data = await response.json();
+            console.log(data)
+        } catch (e) {
+            console.log(e)
+        }
     },
 }
