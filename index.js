21b045da18100d260f274df9dbe5dd37f9850411
Oke Amuwha <amuwhao46@gmail.com>
2022-10-16 18:28:48 -0400

    Implemented a weather API to display the current weather

--- index.js
+++ index.js
@@ -12,10 +12,31 @@ const API = {
     },
 }
 
-window.onload = function() {
-    API.getRandomDog()
+
+// Weather API Wrapper
+const WeatherAPI = {
+    APIKEY: '3d62fad2cdea331d9c871600eda4d585',
+    getCurrentWeather() {
+        let lat = 42.3314;
+        let lon = 83.0458;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.APIKEY}&units=imperial`,
+        { method: 'GET' })
+        .then(response => response.json())
+        .then((data) => { 
+            data = { data }
+            let city = "Detroit";
+            let state = "MI";
+            let temp = data.data.main.temp;
+            document.querySelector("#weather").innerHTML = `<b>${city}</b> <br> <b>${state}</b> <br> ${temp}`
+            console.log(temp)
+        })
+    }
 }
 
+window.onload = function() {
+    API.getRandomDog()
+    WeatherAPI.getCurrentWeather()
+}
 
 document.addEventListener("DOMContentLoaded", function(){
   // add padding top to show content behind navbar
