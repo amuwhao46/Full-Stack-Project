60b79357b5419b15f5af5ccf8a2d975b071abfce
Oke Amuwha <amuwhao46@gmail.com>
2022-10-16 17:24:56 -0400

    Updated file to use Bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // API Wrapper
 const API = {
     async getRandomDog() {
+        let dogButton = document.querySelector("#dogStatus")
+        dogButton.innerHTML = ""
         fetch("https://random.dog/woof.json").then(response => response.json()).then(function (data) {
             const dog = data.url //Handles dog pic or video
             const isVideo = dog.split('.').pop() == "mp4" //splits last element in array
@@ -13,3 +15,11 @@ const API = {
 window.onload = function() {
     API.getRandomDog()
 }
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  // add padding top to show content behind navbar
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+}); 
+// DOMContentLoaded  end
